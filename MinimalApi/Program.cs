80df57171351d52726e4edd90987cf918bac4dfe
Casper <caspervddijk@gmail.com>
2023-06-09 17:51:47 +0200

    Added Auth0 authentication and swagger fix

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.OpenApi.Models;
 using MinimalApi;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,8 +10,56 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddCors();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] {}
+            }
+        });
+});
 builder.Services.AddDbContext<DataContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("DefaultCon")));
+
+IdentityModelEventSource.ShowPII = true;
+
+// Configure authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, c =>
+    {
+        c.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+        c.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidAudience = builder.Configuration["Auth0:Audience"],
+            ValidIssuer = $"{builder.Configuration["Auth0:Domain"]}"
+        };
+    });
+
+// Configure authorization
+builder.Services.AddAuthorization(o =>
+{
+    o.AddPolicy("team:read-write", p => p
+        .RequireAuthenticatedUser()
+        .RequireClaim("scope", "team:read-write"));
+});
 
 var app = builder.Build();
 
@@ -16,11 +67,34 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors(options =>
+    options.WithOrigins("http://localhost:5173")
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+
+app.UseCors(options =>
+    options.WithOrigins("http://localhost:7020")
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapGroup("Team")
-    .MapTeamApi();
+    .MapTeamApi()
+    .RequireAuthorization("team:read-write");
 
 #region Startup
 app.UseHttpsRedirection();
